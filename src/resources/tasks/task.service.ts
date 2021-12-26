9c4bc038b2f87c3b5380142b958548acf4f16220
evitla <s.dinmuhamed@gmail.com>
2021-12-27 02:44:43 +0600

    feat: log info for task

--- src/resources/tasks/task.service.ts
+++ src/resources/tasks/task.service.ts
@@ -3,12 +3,26 @@ import { TaskRequest } from '../../common/types';
 import * as tasksRepo from './task.memory.repository';
 
 export const getAll = async (req: TaskRequest, reply: FastifyReply) => {
+  req.log.info({
+    url: req.raw.url,
+    method: req.method,
+    statusCode: reply.raw.statusCode,
+  });
+
   const tasks = await tasksRepo.getAllTasks();
 
   reply.send(tasks);
 };
 
 export const getById = async (req: TaskRequest, reply: FastifyReply) => {
+  req.log.info({
+    url: req.raw.url,
+    boardId: req.params.boardId,
+    taskId: req.params.id,
+    method: req.method,
+    statusCode: reply.raw.statusCode,
+  });
+
   try {
     const task = await tasksRepo.getTaskById(req.params.boardId, req.params.id);
     reply.send(task);
@@ -19,10 +33,26 @@ export const getById = async (req: TaskRequest, reply: FastifyReply) => {
 
 export const create = async (req: TaskRequest, reply: FastifyReply) => {
   const task = await tasksRepo.createTask(req.params.boardId, req.body);
+  req.log.info({
+    url: req.raw.url,
+    boardId: req.params.boardId,
+    method: req.method,
+    statusCode: reply.raw.statusCode,
+    body: task,
+  });
+
   reply.code(201).send(task);
 };
 
 export const remove = async (req: TaskRequest, reply: FastifyReply) => {
+  req.log.info({
+    url: req.raw.url,
+    boardId: req.params.boardId,
+    taskId: req.params.id,
+    method: req.method,
+    statusCode: reply.raw.statusCode,
+  });
+
   const message = await tasksRepo.removeTask(req.params.boardId, req.params.id);
   reply.send(message);
 };
@@ -33,5 +63,14 @@ export const update = async (req: TaskRequest, reply: FastifyReply) => {
     req.params.id,
     req.body
   );
+
+  req.log.info({
+    url: req.raw.url,
+    boardId: req.params.boardId,
+    taskId: req.params.id,
+    method: req.method,
+    statusCode: reply.raw.statusCode,
+    body: updatedtask,
+  });
   reply.send(updatedtask);
 };
